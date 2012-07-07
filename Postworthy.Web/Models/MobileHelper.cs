4ca9e2e34b3b2280397a91579b99816da0fc29a4
keyj63 <landon.key@gmail.com>
2012-07-07 17:11:52 -0500

    Updated mobile styling and added more mobile browsers to the list.

--- Postworthy.Web/Models/MobileHelper.cs
+++ Postworthy.Web/Models/MobileHelper.cs
@@ -7,7 +7,7 @@ namespace Postworthy.Web.Models
 {
     public class MobileHelper
     {
-        private static string[] mobileDevices = new string[] { "iphone", "ppc", "windows ce", "blackberry", "opera mini", "mobile", "palm", "portable", "opera mobi" };
+        private static string[] mobileDevices = new string[] { "iphone", "ipod", "android", "ppc", "windows ce", "blackberry", "opera mini", "mobile", "palm", "portable", "opera mobi" };
 
         public static bool IsMobileDevice(string userAgent)
         {
