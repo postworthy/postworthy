9d0cc4beeda892f085e69b6bf7811b06c0f24315
keyj63 <landon.key@gmail.com>
2012-07-10 16:00:19 -0500

    Modifications for mobile Prompt to save to home screen

---
+++ Postworthy.Web/Scripts/bookmark_bubble_show.js
@@ -0,0 +1,35 @@
+﻿window.addEventListener('load', function () {
+    window.setTimeout(function () {
+        var bubble = new google.bookmarkbubble.Bubble();
+
+        var parameter = 'bmb=1';
+
+        bubble.hasHashParameter = function () {
+            return window.location.hash.indexOf(parameter) != -1;
+        };
+
+        bubble.setHashParameter = function () {
+            if (!this.hasHashParameter()) {
+                window.location.hash += parameter;
+            }
+        };
+
+        bubble.getViewportHeight = function () {
+            return window.innerHeight;
+        };
+
+        bubble.getViewportScrollY = function () {
+            return window.pageYOffset;
+        };
+
+        bubble.registerScrollHandler = function (handler) {
+            window.addEventListener('scroll', handler, false);
+        };
+
+        bubble.deregisterScrollHandler = function (handler) {
+            window.removeEventListener('scroll', handler, false);
+        };
+
+        bubble.showIfAllowed();
+    }, 1000);
+}, false);
