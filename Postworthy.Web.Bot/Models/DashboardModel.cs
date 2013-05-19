1d16998845301e90cd1a2222877b2024c03b9018
Landon Key <landon.key+pw@gmail.com>
2013-05-18 23:25:18 -0500

    DashboardModel Update

--- Postworthy.Web.Bot/Models/DashboardModel.cs
+++ Postworthy.Web.Bot/Models/DashboardModel.cs
@@ -9,6 +9,7 @@ namespace Postworthy.Web.Bot.Models
     public class DashboardModel
     {
         public DateTime BotStartupTime { get; set; }
+        public TimeSpan Runtime { get { return DateTime.Now - BotStartupTime; } }
         public DateTime LastTweetTime { get; set; }
         public int TweetsSentSinceLastFriendRequest { get; set; }
         public List<Tweet> PotentialTweets { get; set; }
@@ -16,6 +17,17 @@ namespace Postworthy.Web.Bot.Models
         public List<Tweet> Tweeted { get; set; }
         public List<KeyValuePair<string, int>> KeywordSuggestions { get; set; }
         public List<KeyValuePair<Tweep, int>> PotentialTweeps { get; set; } ///TODO: Replace Tuple with CountableItem
+        public double TweetsPerHour { get; set; }
+        public double[] TweetsPerHourLast24 { get; set; }
+        public List<KeyValuePair<Tweep, int>> TopFriendTweetCounts { get; set; }
+        public int MinimumRetweetLevel { get; set; }
+        public int CurrentClout { get; set; }
+        public int FollowerCount { get; set; }
+        public int FollowingCount { get; set; }
+        public List<KeyValuePair<string, int>> KeywordsWithOccurrenceCount { get; set; }
+        public List<KeyValuePair<string, int>> PotentialKeywordsWithOccurrenceCount { get; set; }
+        public double TwitterStreamVolume { get; set; }
+
 
         public DashboardModel()
         {
@@ -25,6 +37,10 @@ namespace Postworthy.Web.Bot.Models
             PotentialTweeps = new List<KeyValuePair<Tweep, int>>();
             LastTweetTime = DateTime.MaxValue;
             KeywordSuggestions = new List<KeyValuePair<string, int>>();
+            TweetsPerHourLast24 = new double[24];
+            TopFriendTweetCounts = new List<KeyValuePair<Tweep, int>>();
+            KeywordsWithOccurrenceCount = new List<KeyValuePair<string, int>>();
+            PotentialKeywordsWithOccurrenceCount = new List<KeyValuePair<string, int>>();
 
             LoadFromRepository();
         }
