dbd291c0ae7a8de8e0e589eb6d9c5428aa5ddfcd
Landon Key <landon.key+pw@gmail.com>
2013-05-19 21:52:15 -0500

    Populate data from runtime settings
    This code should be completed but it is late and I am going to bed ;-)

--- Postworthy.Web.Bot/Models/DashboardModel.cs
+++ Postworthy.Web.Bot/Models/DashboardModel.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Postworthy.Models.Account;
+using Postworthy.Models.Repository;
 using Postworthy.Models.Twitter;
+using Postworthy.Tasks.Bot.Settings;
 
 namespace Postworthy.Web.Bot.Models
 {
     public class DashboardModel
     {
+        private const string RUNTIME_REPO_KEY = "TweetBotRuntimeSettings";
+
+        public PostworthyUser User { get; set; }
         public DateTime BotStartupTime { get; set; }
         public TimeSpan Runtime { get { return DateTime.Now - BotStartupTime; } }
         public DateTime LastTweetTime { get; set; }
@@ -16,7 +22,7 @@ namespace Postworthy.Web.Bot.Models
         public List<Tweet> PotentialReTweets { get; set; }
         public List<Tweet> Tweeted { get; set; }
         public List<KeyValuePair<string, int>> KeywordSuggestions { get; set; }
-        public List<KeyValuePair<Tweep, int>> PotentialTweeps { get; set; } ///TODO: Replace Tuple with CountableItem
+        public List<KeyValuePair<Tweep, int>> PotentialFriendRequests { get; set; }
         public double TweetsPerHour { get; set; }
         public double[] TweetsPerHourLast24 { get; set; }
         public List<KeyValuePair<Tweep, int>> TopFriendTweetCounts { get; set; }
@@ -29,12 +35,13 @@ namespace Postworthy.Web.Bot.Models
         public double TwitterStreamVolume { get; set; }
 
 
-        public DashboardModel()
+        public DashboardModel(PostworthyUser user)
         {
+            User = user;
             PotentialTweets = new List<Tweet>();
             PotentialReTweets = new List<Tweet>();
             Tweeted = new List<Tweet>();
-            PotentialTweeps = new List<KeyValuePair<Tweep, int>>();
+            PotentialFriendRequests = new List<KeyValuePair<Tweep, int>>();
             LastTweetTime = DateTime.MaxValue;
             KeywordSuggestions = new List<KeyValuePair<string, int>>();
             TweetsPerHourLast24 = new double[24];
@@ -45,9 +52,31 @@ namespace Postworthy.Web.Bot.Models
             LoadFromRepository();
         }
 
+        private string RepoKey
+        {
+            get
+            {
+                return User.TwitterScreenName + "_" + RUNTIME_REPO_KEY;
+            }
+        }
+
         private void LoadFromRepository()
         {
-            //Load data from repository (i.e. TweetBotRuntimeSettings)
+            Repository<TweetBotRuntimeSettings> repo = Repository<TweetBotRuntimeSettings>.Instance;
+
+            var runtimeSettings = (repo.Query(RepoKey) ?? new List<TweetBotRuntimeSettings> { new TweetBotRuntimeSettings() }).FirstOrDefault();
+
+            if (runtimeSettings != null)
+            {
+                PotentialTweets = runtimeSettings.PotentialTweets;
+                PotentialReTweets = runtimeSettings.PotentialReTweets;
+                Tweeted = runtimeSettings.Tweeted;
+                PotentialFriendRequests = runtimeSettings.PotentialFriendRequests.Select(x => new KeyValuePair<Tweep, int>(x.Key, x.Count)).ToList();
+                LastTweetTime = runtimeSettings.LastTweetTime;
+                ///TODO: Add the rest...
+
+                //Load data from repository (i.e. TweetBotRuntimeSettings)
+            }
         }
     }
 }
