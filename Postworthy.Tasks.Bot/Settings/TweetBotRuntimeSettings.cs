1899016363b385fbd1e85d3aaf5403ae02783684
Landon <landon.key@gmail.com>
2013-05-23 14:58:52 -0500

    Dynamic adjustment of MinimumRetweetLevel

--- Postworthy.Tasks.Bot/Settings/TweetBotRuntimeSettings.cs
+++ Postworthy.Tasks.Bot/Settings/TweetBotRuntimeSettings.cs
@@ -33,13 +33,14 @@ namespace Postworthy.Tasks.Bot.Settings
             {
                 if (this.Tweeted != null && this.Tweeted.Count > 5)
                 {
+                    double less = 0.2 + Math.Max(60.0 - ((DateTime.Now - LastTweetTime).TotalMinutes / 2), 0); //Allows us to progressivly lower the bar of what we accept over time
                     double stdev = 0;
                     var values = this.Tweeted.Select(x => x.RetweetCount);
                     double avg = values.Average();
                     //Get Standard Deviation
                     stdev = Math.Sqrt(values.Sum(d => (d - avg) * (d - avg)) / values.Count());
 
-                    return values.Where(x => x <= (avg + stdev * 2) && x >= (avg - stdev * 2)).Average() * 0.65;
+                    return values.Where(x => x <= (avg + stdev * 2) && x >= (avg - stdev * 2)).Average() * less;
                 }
 
                 return 2.0;
