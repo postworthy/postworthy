2ca00bb85d67bd7aab4fa10e13a79bc4e0564f9d
Landon Key <landon.key+pw@gmail.com>
2013-05-24 13:29:19 -0500

    ignoreList Bug fix
    there was a bug due to the ignorelist also being the list that was
    updated with new keywords. this could cause the IKeywordSuggestionStep
    to actually update its own ignore list which we do not want to happen in
    this way.

--- Postworthy.Tasks.StreamMonitor/DualStreamMonitor.cs
+++ Postworthy.Tasks.StreamMonitor/DualStreamMonitor.cs
@@ -247,6 +247,7 @@ namespace Postworthy.Tasks.StreamMonitor
                     }
                     else
                     {
+                        var ignoreList = !string.IsNullOrEmpty(track) ? track.ToLower().Split(',').ToList() : new List<string>();
                         trackList = !string.IsNullOrEmpty(track) ? track.ToLower().Split(',').ToList() : new List<string>();
 
                         if (processingStep is IKeywordSuggestionStep)
@@ -257,7 +258,7 @@ namespace Postworthy.Tasks.StreamMonitor
                                 /* I have chosen to wrap these calls in seperate try catch statements incase one fails
                                  * the other can still run. This way if the get fails we may still have hope of a reset.
                                  */
-                                try { keywordSuggestionStep.SetIgnoreKeywords(trackList); } catch { }
+                                try { keywordSuggestionStep.SetIgnoreKeywords(ignoreList); } catch { }
                                 try { trackList.AddRange(keywordSuggestionStep.GetKeywordSuggestions().Select(x=>x.ToLower()).ToList()); } catch { }
                                 try { keywordSuggestionStep.ResetHasNewKeywordSuggestions(); } catch { }
                             }
