de87a6e440d7fc64d78a0538863bce2087d04665
Landon Key <landon.key+pw@gmail.com>
2013-06-17 21:28:14 -0500

    Dashboard Updates

--- Postworthy.Web.Bot/Scripts/squaredUp.js
+++ Postworthy.Web.Bot/Scripts/squaredUp.js
@@ -2,7 +2,6 @@
 Author: Landon Key
 This script looks for data-squared attribute and makes the container squared based on the property set 
 (i.e. - height, width).
-
 */
 
 $(document).ready(function () {
@@ -10,14 +9,16 @@ $(document).ready(function () {
         o = $(o);
         var max = parseFloat(o.attr("data-squared-max") ? o.attr("data-squared-max") : "0");
         var changeAttr = o.attr("data-squared").toLowerCase();
-        var matchAttr = changeAttr == "width" ? "height" : "width";
-        var oldContentSize = o[changeAttr]();
-        var newContentSize = max > 0 && o[matchAttr]() > max ? max : o[matchAttr]();
-        o.css(changeAttr, newContentSize + "px");
+        if (changeAttr == "width" || changeAttr == "height") {
+            var matchAttr = changeAttr == "width" ? "height" : "width";
+            var oldContentSize = o[changeAttr]();
+            var newContentSize = max > 0 && o[matchAttr]() > max ? max : o[matchAttr]();
+            o.css(changeAttr, newContentSize + "px");
 
-        if (changeAttr == "height") {
-            if (newContentSize - oldContentSize > 0)
-                o.css("padding-top", (newContentSize - oldContentSize) / 2);
+            if (changeAttr == "height") {
+                if (newContentSize - oldContentSize > 0)
+                    o.css("padding-top", (newContentSize - oldContentSize) / 2);
+            }
         }
     });
 });
