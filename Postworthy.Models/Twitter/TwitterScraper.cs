ffff45771fcbdca3fcfa8c039985de4cbc03d73c
Landon Key <landon.key+pw@gmail.com>
2013-06-21 23:32:42 -0500

    TwitterScraper Added
    This tool will be used to scrape content for the primary user that is
    not available through the api or is not accessible through the api as we
    need it.

---
+++ Postworthy.Models/Twitter/TwitterScraper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using HtmlAgilityPack;
+
+namespace Postworthy.Models.Twitter
+{
+    public static class TwitterScraper
+    {
+        public static string GetTwitterUrl(string url)
+        {
+            if (string.IsNullOrEmpty(ConfigurationManager.AppSettings["PrimaryUser"]) || string.IsNullOrEmpty(ConfigurationManager.AppSettings["Password"]))
+                throw new Exception("PrimaryUser and Password settings must be present in the config file to use GetTwitterUrl");
+
+            string result = string.Empty;
+            CookieContainer cookieContainer = new CookieContainer();
+            HttpWebRequest initialFetch = (HttpWebRequest)WebRequest.Create("http://twitter.com");
+            initialFetch.CookieContainer = cookieContainer;
+
+            using (var initialResponse = initialFetch.GetResponse())
+            {
+                var doc = new HtmlDocument();
+                HtmlNode token = null;
+                using (var reader = new StreamReader(initialResponse.GetResponseStream()))
+                {
+                    doc.Load(reader);
+                    token = doc.DocumentNode.SelectNodes("//input[@name='authenticity_token']").FirstOrDefault();
+                }
+                
+                var authenticityToken = token.GetAttributeValue("value", "");
+                if (!string.IsNullOrEmpty(authenticityToken))
+                {
+                    HttpWebRequest loginRequest = (HttpWebRequest)WebRequest.Create("https://twitter.com/sessions");
+                    loginRequest.CookieContainer = cookieContainer;
+
+                    loginRequest.Method = "POST";
+                    loginRequest.ContentType = "application/x-www-form-urlencoded";
+
+                    string postData = "session%5Busername_or_email%5D=" + ConfigurationManager.AppSettings["PrimaryUser"];
+                    postData += "&session%5Bpassword%5D=" + ConfigurationManager.AppSettings["Password"];
+                    postData += "&authenticity_token=" + authenticityToken;
+                    postData += "&redirect_after_login=" + HttpUtility.UrlEncode(url);
+                    byte[] data = new ASCIIEncoding().GetBytes(postData);
+
+                    loginRequest.ContentLength = data.Length;
+
+                    using (Stream stream = loginRequest.GetRequestStream())
+                    {
+                        stream.Write(data, 0, data.Length);
+                    }
+
+                    using (var loginResponse = (HttpWebResponse)loginRequest.GetResponse())
+                    {
+                        using (var reader = new StreamReader(loginResponse.GetResponseStream()))
+                        {
+                            return reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
